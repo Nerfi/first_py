75950026270b9a2f62be649b08b8b0ae849adc6b
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-11-26 19:32:23 +0100

    .

--- pdf.py
+++ pdf.py
@@ -2,29 +2,31 @@
 from PyPDF2 import PdfFileReader,PdfFileWriter
 
 
-def PdfReader(page):
-  #creatign the pdf
-    pdftext = "dummy-pdf_2.pdf"
+def PdfReader(number):
+  #reading the user input, not sure if is like this
+  select = input(number)
 
-    with open(pdftext, 'rb') as textpdf:
+  #creatign the pdf
+  pdftext = "/home/juanfran/Downloads/juanlinkedingresume.pdf"
+
+  with open(pdftext, 'rb') as textpdf:
     #reading the PDF
       reader = PdfFileReader(textpdf)
     #getting the num of pages of the pdf file
-    for page in range(reader.getNumPages()):
-        default_page = reader.getPage(0) #getting the first page of the pdf as default apge when it gets open
+      for page in range(reader.getNumPages()):
+        default_page = reader.getPage(0) #getting the first page of the pdf as default page when it gets open
         #not sure this is the right way
-        if page > 0:
-          merge_page = default_page.addPage(page +1 )
+        if (select.isdigit()):
+          merge_page = default_page.addPage(page + 1 )
           return merge_page
 
 
 
 
     #checking that we closde the file if not, we do so
-    if not textpdf.closed:
+  if not textpdf.closed:
       textpdf.close()
       print "closed"
-
 
 
 
