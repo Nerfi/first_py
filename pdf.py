0972428f0f04091babc8d9d1a73d51eaf31eeac3
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-11-27 17:49:42 +0100

    made some progress

--- pdf.py
+++ pdf.py
@@ -2,32 +2,40 @@
 from PyPDF2 import PdfFileReader,PdfFileWriter
 
 
-def PdfReader(number):
-  #reading the user input, not sure if is like this
-  select = input(number)
-
+def PdfReader(number,pdf):
   #creatign the pdf
   pdftext = "/home/juanfran/Downloads/juanlinkedingresume.pdf"
+
+  #with =  context manager, dont need to close the file
+  #when using context manager, ptyhon will do that for us
 
   with open(pdftext, 'rb') as textpdf:
     #reading the PDF
       reader = PdfFileReader(textpdf)
-    #getting the num of pages of the pdf file
-      for page in range(reader.getNumPages()):
-        default_page = reader.getPage(0) #getting the first page of the pdf as default page when it gets open
-        #not sure this is the right way
-        if (select.isdigit()):
-          merge_page = default_page.addPage(page + 1 )
-          return merge_page
+      page = reader.getPage(number) #take the page the user wants
+      with open("new.pdf","wb") as f:
+        writer = PdfFileWriter() #creates the pdf writer
+        writer.addPage(page)
+        writer.write(f)
+
+  if __name__ == "__main__":
+    PdfReader(0)
 
 
+#importing the module
+from PyPDF2 import PdfFileReader,PdfFileWriter
 
+def PdfReader(number,pdf,new_pdf):
+  #Original pdf to read
 
-    #checking that we closde the file if not, we do so
-  if not textpdf.closed:
-      textpdf.close()
-      print "closed"
+  with open(pdf, 'rb') as textpdf: # open the pdf (will close automatically)
+      reader = PdfFileReader(textpdf) # create the reader
+      page = reader.getPage(number) # grab the page we want
+      with open(new._pdf,"wb") as f: # create the new pdf
+        writer = PdfFileWriter() # create the pdf writer
+        writer.addPage(page) # add the page to the buffer
+        writer.write(f) # write the new pdf to the filestream
 
-
-
+if __name__ == "__main__": # magic to run the method automatically
+    PdfReader(0) # call the code we wrote, get the first page
 
