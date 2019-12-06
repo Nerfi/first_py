b2dd0052ef2037ce13eb80e998a2d323c574693b
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-12-06 10:27:59 +0100

    made some changes

--- pdf.py
+++ pdf.py
@@ -2,18 +2,18 @@
 from PyPDF2 import PdfFileReader,PdfFileWriter
 
 
-def PdfReader(number,pdf):
+def PdfReader(number,pdf,new_pdf):
   #creatign the pdf
-  pdftext = "/home/juanfran/Downloads/juanlinkedingresume.pdf"
+  #pdftext = "/home/juanfran/Downloads/juanlinkedingresume.pdf"
 
   #with =  context manager, dont need to close the file
   #when using context manager, ptyhon will do that for us
 
-  with open(pdftext, 'rb') as textpdf:
+  with open(pdf, 'rb') as textpdf:
     #reading the PDF
       reader = PdfFileReader(textpdf)
       page = reader.getPage(number) #take the page the user wants
-      with open("new.pdf","wb") as f:
+      with open(new_pdf,"wb") as f:
         writer = PdfFileWriter() #creates the pdf writer
         writer.addPage(page)
         writer.write(f)
